0ec15f5cf774ab7bea5d3cec92dfce780952664d
Simipro <sihuber@gmx.net>
2015-04-10 21:17:07 +0200

    gogo

--- src/neuronalnet/Application.scala
+++ src/neuronalnet/Application.scala
@@ -11,14 +11,85 @@ import scala.util.Random
 object Application {
   def main(args: Array[String]) {
     val neuronalNet = new Net()
-    neuronalNet.input(0, 0)
+
+    var trainData = new mutable.MutableList[TrainSet]
+    for (i <- 1 to 100) {
+      trainData += TrainSet(0,0,0)
+    }
+    for (i <- 1 to 100) {
+      trainData += TrainSet(0,1,0)
+    }
+    for (i <- 1 to 100) {
+      trainData += TrainSet(1,0,0)
+    }
+    for (i <- 1 to 100) {
+      trainData += TrainSet(1,1,1)
+    }
   }
 }
 
+case class TrainSet(x1:Int, x2:Int, y:Int)
+
 class Net {
+
+  def setResult(y: Int) = {
+    outputLayer.neurons.apply(0).setResult(y)
+  }
+
+  def train(trainData: List[TrainSet]) = {
+    var J = 0.0
+    trainData.foreach(D => {
+      input(D.x1, D.x2)
+      val a_3 = outputLayer.getResult()
+      val cost = -D.y*math.log(a_3) - (1 - D.y)*math.log(1 - a_3)
+      J = J + cost
+    })
+    J = J / trainData.size
+
+    val inputError = Array[Double](3)
+    val hiddenError = Array[Double](2)
+    trainData.foreach(D => {
+      input(D.x1, D.x2)
+      setResult(D.y)
+
+
+      // Input Layer
+      inputError(0) = inputError(0) + inputLayer.neurons.apply(0).getError()
+      inputError(1) = inputError(1) + inputLayer.neurons.apply(1).getError()
+      inputError(2) = inputError(2) + inputLayer.neurons.apply(2).getError()
+      //
+
+      // Hidden Layer
+      hiddenError(0) = hiddenError(0) + hiddenLayer.neurons.apply(0).getError()
+      hiddenError(1) = hiddenError(1) + hiddenLayer.neurons.apply(1).getError()
+
+      //
+
+    })
+
+    // learning rate alpha
+    val alpha = 0.9
+    // new weights
+    for (i <- 0 until inputError.length) {
+      val newWeight = inputError(i)/trainData.size
+      inputLayer.neurons.apply(i).postNeurons.apply(0).weight = inputLayer.neurons.apply(i).postNeurons.apply(0).weight - alpha*newWeight
+    }
+    for (i <- 0 until hiddenError.length) {
+      val newWeight = hiddenError(i)/trainData.size
+
+    }
+
+    J = J / trainData.size
+
+
+
+
+
+  }
+
   def input(input1: Int, input2: Int): Unit = {
     // trigger bias with 0 value
-    inputLayer.neurons.apply(0).setValue(0)
+    inputLayer.neurons.apply(0).setValue(1)
     inputLayer.neurons.apply(1).setValue(input1)
     inputLayer.neurons.apply(2).setValue(input2)
   }
@@ -51,7 +122,8 @@ abstract class Layer(units:Int) {
 class InputLayer(units:Int) extends Layer(units) {
   override def createUnits(): Unit = {
     // add bias neuron first of all
-    neurons += new BiasNeuron
+    // in case of input layer this is a normal inputNeuron which we fill with 1 everytime
+    neurons += new InputNeuron
     for (x <- 1 to units) {
       neurons += new InputNeuron
     }
@@ -76,6 +148,12 @@ class HiddenLayer(units:Int, preLayer: Layer) extends Layer(units) {
   }
 }
 class OutputLayer(units:Int,preLayer: Layer) extends Layer(units) {
+
+  def getResult():Double= {
+      neurons.apply(0).value
+  }
+
+
   override def createUnits(): Unit = {
     for (x <- 1 to units) {
       neurons += new OutputNeuron
@@ -88,6 +166,7 @@ class OutputLayer(units:Int,preLayer: Layer) extends Layer(units) {
     })
   }
 }
+
 class BiasNeuron extends Neuron {
   override def setValue(value: Double): Unit = {
     this.value = 1
@@ -99,9 +178,9 @@ class BiasNeuron extends Neuron {
       case ActivationWeight.exeeded => {
         increaseCurrentActivationValue()
         // trigger next connections
-        neurons.foreach(C => {
+        postNeurons.foreach(C => {
           // cause im a bias 1*
-          C.neuron.trigger(1*C.weight)
+          C.postNeuron.trigger(1*C.weight)
         })
       }
       case ActivationWeight.tolow => {
@@ -112,12 +191,22 @@ class BiasNeuron extends Neuron {
       }
     }
   }
+
+  override def setResult(error: Double): Unit = {
+    // bias unit has no delta but an error value to correct
+    // TODO: Check this again really need an error @ bias neuron ?
+    setError(error*1)
+
+  }
 }
 
 
 
 
 class OutputNeuron extends Neuron  {
+  var result:Double = 0
+
+
   override def setValue(value: Double): Unit = {
     this.value = this.value + value
   }
@@ -132,6 +221,7 @@ class OutputNeuron extends Neuron  {
         increaseCurrentActivationValue()
         setValue(newValue)
         val result = MathHelper.sigmoid(value)
+        value = result
         println(" Jepaa we finished forward Prop with result: " + result)
       }
       case ActivationWeight.reset => {
@@ -139,6 +229,13 @@ class OutputNeuron extends Neuron  {
         setValue(newValue)
       }
     }
+  }
+
+  override def setResult(y: Double): Unit ={
+    val delta = value - y
+    preNeurons.foreach(C => {
+      C.preNeuron.setResult(delta*C.weight)
+    })
   }
 }
 
@@ -154,9 +251,10 @@ class HiddenNeuron extends Neuron {
           increaseCurrentActivationValue()
           setValue(value + newValue)
           val sigValue = MathHelper.sigmoid(value)
+          finalValue = sigValue
           // trigger next connections
-          neurons.foreach(C => {
-            C.neuron.trigger(sigValue*C.weight)
+          postNeurons.foreach(C => {
+            C.postNeuron.trigger(sigValue*C.weight)
           })
         }
         case ActivationWeight.tolow => {
@@ -174,14 +272,27 @@ class HiddenNeuron extends Neuron {
   override def setValue(value: Double): Unit = {
     this.value = this.value + value
   }
+
+
+
+  override def setResult(error: Double): Unit = {
+    // set theta based on error from post neuron
+    setError(error*finalValue)
+
+    val delta = error*MathHelper.sigmoidGradient(value)
+
+    postNeurons.foreach(C => {
+        C.preNeuron
+    })
+  }
 }
 
 
 class InputNeuron extends Neuron {
 
   override def trigger(newValue:Double, activationWeight: ActivationWeight): Unit = {
-    neurons.foreach(N =>  {
-      N.neuron.trigger(newValue*N.weight)
+    postNeurons.foreach(N =>  {
+      N.postNeuron.trigger(newValue*N.weight)
     })
   }
 
@@ -189,9 +300,13 @@ class InputNeuron extends Neuron {
     this.value = value
     trigger(value)
   }
+
+  override def setResult(error: Double): Unit = {
+    setError(error*finalValue)
+  }
 }
 
-case class Connection(neuron: Neuron, weight:Double)
+case class Connection(postNeuron: Neuron, preNeuron:Neuron, var weight:Double)
 
 
 object ActivationWeight extends Enumeration {
@@ -200,17 +315,29 @@ object ActivationWeight extends Enumeration {
 }
 
 abstract class Neuron {
+  def setResult(y: Double)
+
   var value = 0.0
+  var finalValue = 0.0
   var activationValue = 0.0
   var currentActivationValue = 0.0
-  var neurons:mutable.MutableList[Connection] = mutable.MutableList[Connection]()
+  var postNeurons:mutable.MutableList[Connection] = mutable.MutableList[Connection]()
+  var preNeurons:mutable.MutableList[Connection] = mutable.MutableList[Connection]()
+  var error = 0.0
 
+  def setError(error: Double) = {
+    this.error = error
+  }
 
+  def getError():Double= {
+    return this.error
+  }
 
   def setValue(value:Double)
 
   def resetValues(): Unit = {
     this.value = 0
+    this.finalValue = 0
     this.currentActivationValue = 0
   }
 
@@ -240,12 +367,14 @@ abstract class Neuron {
 
   /**
    * it registers the neuron and set his activation weight +1
-   * @param neuron
+   * @param postNeuron
    * @return
    */
-  def register(neuron:Neuron) = {
-    neuron.increaseActivationValue()
-    neurons += Connection(neuron, MathHelper.randomInitializedNumber())
+  def register(postNeuron:Neuron) = {
+    postNeuron.increaseActivationValue()
+    var connection = Connection(postNeuron, this, MathHelper.randomInitializedNumber())
+    postNeurons += connection
+    postNeuron.preNeurons += connection
   }
 
   override def toString():String = {
@@ -254,6 +383,10 @@ abstract class Neuron {
 }
 
 object MathHelper {
+  def sigmoidGradient(z: Double) = {
+    sigmoid(z) * (1 - sigmoid(z))
+  }
+
   var random:Random = new Random()
 
   /*
